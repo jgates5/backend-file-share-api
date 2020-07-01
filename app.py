be38b783a1a6763f7a2e1dd5fbfaa90856b5a5bf
jeffwgates <jeff.gates0507@gmail.com>
2020-07-01 12:12:23 -0600

    Added app route for delete button and download

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_heroku import Heroku
 import io
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://trbtezwjfndjvn:17011eddb20512357f5c2dce610c738a26ae0430f390320c104bbb4cbbdd3041@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d6baahc4q84km5"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -32,5 +32,36 @@ class FileSchema(ma.Schema):
 file_schema = FileSchema()
 files_schema = FileSchema(many=True)
 
+
+@app.route("/file/add", methods=["POST"])
+def add_file():
+  name = request.form.get("name")
+  file_type = request.form.get("type")
+  data = request.files.get("data")
+
+  new_file = File(name, file_type, data.read())
+  db.session.add(new_file)
+  db.session.commit()
+
+  return jsonify("File added successfully")
+
+@app.route("/file/get/data", methods=["GET"])
+def get_file_data():
+  file_data = db.session.query(File).all()
+  return jsonify(files_schema.dump(file_data))
+
+@app.route("/file/get/<id>", methods=["GET"])
+def get_file(id):
+  file_data = db.session.query(File).filter(File.id == id).first()
+  return send_file(io.BytesIO(file_data.data), 
+    attachment_filename=file_data.name, 
+    mimetype=file_data.file_type)
+
+@app.route("/file/delete/<id>", methods=["DELETE"])
+def delete_file(id):
+  file_data = db.session.query(File).filter(File.id == id).first()
+  db.session.delete(file_data)
+  db.session.commit()
+  return jsonify("File Deleted Successfully")
 if __name__ == "__main__":
   app.run(debug=True)
